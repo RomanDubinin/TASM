5b575aaf6648a4b9d7458e89af4f9c02f812d45a
romandubinin <roman230394@yandex.ru>
2014-11-08 00:46:50 +0600

    передача параметров через stack

--- 1task/1task.asm
+++ 1task/1task.asm
@@ -1,36 +1,39 @@
 dseg segment
-	; определение данных
-	mas dw 0, -20, 13, 14, -88
-	masSize dw 5
-	mes db ', $'
+    ; определение данных
+    mas dw 0, -20, 13, 14, -88
+    masSize dw 5
+    mes db ', $'
 dseg ends
 
 sseg segment stack
-	n dw 256 dup (?)
+    n dw 256 dup (?)
 sseg ends
 
 cseg segment
-	
-	assume ds: dseg, cs: cseg, ss: sseg
-	include OutInt.inc
+
+    assume ds: dseg, cs: cseg, ss: sseg
+    include OutInt.inc
 start: 
-	mov ax, dseg
-	mov ds, ax
-	mov si, 0
-	mov cx, masSize
+    mov ax, dseg
+    mov ds, ax
+    mov si, 0
+    mov cx, masSize
 cycle:
-	dec cx
-	mov ax, mas[si]
-	add si, 2
-	call OutInt
-	mov dx, offset mes
-	mov ah,09h
-	int 21h
-	jcxz endprog
-	jmp cycle
-	
+    
+    push mas[si]
+    call OutInt
+    pop mas[si]
+    mov dx, offset mes
+    mov ah,09h
+    int 21h
+    
+    add si, 2
+    dec cx
+    jcxz endprog
+    jmp cycle
+
 endprog:
-	mov ah,04Ch
-	int 21h
+    mov ah,04Ch
+    int 21h
 cseg ends
 end start
