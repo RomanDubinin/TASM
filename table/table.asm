5e2afdddbf920394dbaf1b08cbaf71c8a6322570
RomanDubinin <Roman230394@yandex.ru>
2015-04-05 17:11:07 +0600

    вывод таблицы с пары

---
+++ table/table.asm
@@ -0,0 +1,177 @@
+;00h - установить видео режим
+;05h - set active display page
+;0Fh - get info: AL - video mode
+;				 AH - characters per line
+;				 BH - active display page num
+
+;сегмент окружения в инвайронмент
+
+.386
+cseg segment use16
+assume CS:cseg, DS:cseg, SS:cseg
+Org 100h  
+@entry: jmp @start
+
+printBX proc
+	pusha
+	mov cx, 4 
+@k:
+	rol bx, 4 ; bx = 0001000000010000 ; cyсle rotate left
+	mov al, bl ; al = 00010000
+	and al, 0fh ; al = 00000000
+	cmp al, 10 
+	sbb al, 69h ; op1 - (op2 + cf)
+	das
+	mov dh, 02h
+	xchg ax, dx
+	int 21h
+	loop @k
+	popa
+	ret
+printBX endp
+
+printVideoMode proc
+	pusha
+	
+	mov ah, 0Fh
+	int 10h
+	
+	mov bl, 0
+	call printBX
+	mov bx, ax
+	call printBX
+	
+	popa
+	ret
+printVideoMode endp
+
+
+IsHexChar proc
+	cmp al, 'A'
+	jl @int
+	
+	cmp al, 'F'
+	jg @int
+	
+	sub al, 'A' - 10
+	clc; clear carry flag
+	ret
+	
+	@int:
+	cmp al, '0'
+	jl @fail
+	
+	cmp al, '9'
+	jg @fail
+	
+	sub al, '0'
+	clc; clear carry flag
+	ret
+	
+	@fail:
+	stc; set  carry flag
+	ret
+IsHexChar endp
+
+
+
+@start:
+	cmp ds:[80h], byte ptr 4
+	jl @paramError
+	cmp ds:[82h], byte ptr '/'
+	jne @paramError
+	
+	mov si, 83h
+	mov cx, 2
+	@loop1:
+	lodsb; ds:si -> al
+	call IsHexChar
+	jc @paramError
+	shl dx, 4
+	or dl, al
+	loop @loop1
+	
+	cmp ds:[80h], byte ptr 8
+	jl @paramError
+	cmp ds:[86h], byte ptr '/'
+	jne @paramError
+	
+	mov si, 87h
+	mov cx, 2
+	@loop2:
+	lodsb; ds:si -> al
+	call IsHexChar
+	jc @paramError
+	shl dx, 4
+	or dl, al
+	loop @loop2
+	
+	mov di, dx; di - first param : second param
+	
+	@doThings:
+	;mov ah, 0Fh
+	;int 10h ; запомнил
+	;mov si, ax
+	
+	; set v mode
+	mov ah, 00h
+	mov al, dh
+	int 10h
+	
+	;set page
+	mov ah, 05h
+	mov al, dl
+	int 10h
+	
+	;call printVideoMode
+	
+	mov bx, dx; page & mode in bx
+	mov bh, bl
+	mov cx, 1
+	xor ax, ax
+	xor dx, dx
+	@sycle:
+
+	;output
+	add dl, cs:[left]
+	add dh, cs:[up]
+	mov ah, 02h
+	int 10h
+	
+	mov ah, 09h
+	mov bl, 12h
+	int 10h
+	
+	sub dl,  cs:[left]
+	sub dh, [up]
+	inc dx
+	inc ax
+	
+	test dl, 0fh
+	jnz @sycle
+	
+	;new line
+	
+	add dx, 0100h
+	xor dl, dl
+	
+	test dh, 0fh
+	jne @sycle
+	
+	
+	@exit:
+	ret
+	
+	@paramError:
+	mov ah, 9h
+	mov dx, offset paramErrorMsg
+	int 21h
+	
+	mov ah, 4ch
+	int 21h
+	
+	paramErrorMsg db 'param error$'
+	left db 12
+	up db 4
+end @entry 
+cs ends
